24be6bfd45342ddcd6bc4b3db5172004181ef8a5
Samuel Tijani <omalitijani@gmail.com>
2020-09-16 17:04:43 +0100

    Define routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { RegistrationComponent } from './user/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/user/registration', pathMatch: 'full' },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [{ path: 'registration', component: RegistrationComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
