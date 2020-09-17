d2572babda53ade766722fc9a7075821c5df5877
Samuel Tijani <omalitijani@gmail.com>
2020-09-17 11:58:58 +0100

    Implement on submit

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from './../../shared/user.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor() { }
+  constructor(public service: UserService, private toastr: ToastrService) {}
 
   ngOnInit(): void {
+    this.service.formModel.reset();
   }
 
+  onSubmit() {
+    this.service.register().subscribe(
+      (res: any) => {
+        if (res.succeeded) {
+          this.service.formModel.reset();
+          this.toastr.success('New user created!', 'Registration successful.');
+        } else {
+          res.errors.forEach((element) => {
+            switch (element.code) {
+              case 'DuplicateUserName':
+                this.toastr.error(
+                  'Username is already taken',
+                  'Registration failed.'
+                );
+                break;
+
+              default:
+                this.toastr.error(element.description, 'Registration failed.');
+                break;
+            }
+          });
+        }
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
