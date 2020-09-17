2c09da0f0f8ab942587b5ef8b2c8c134dc8ff03c
Samuel Tijani <omalitijani@gmail.com>
2020-09-17 15:23:39 +0100

    Implement login component

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/user.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  formModel = {
+    UserName: '',
+    Password: '',
+  };
+  constructor(
+    private service: UserService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    if (localStorage.getItem('token') != null)
+      this.router.navigateByUrl('/home');
   }
 
+  onSubmit(form: NgForm) {
+    this.service.login(form.value).subscribe(
+      (res: any) => {
+        localStorage.setItem('token', res.token);
+        this.router.navigateByUrl('/home');
+      },
+      (err) => {
+        if (err.status == 400)
+          this.toastr.error(
+            'Incorrect username or password.',
+            'Authentication failed.'
+          );
+        else console.log(err);
+      }
+    );
+  }
 }
